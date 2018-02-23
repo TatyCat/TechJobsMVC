4adc4c7513e7e2541c59ede4e00a5a4b35006a3d
TatyCat <williams.tatyanna@gmail.com>
2018-02-23 16:06:40 -0500

    MVC web app - added search all functionality

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,22 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        
+        [HttpPost]
+        public IActionResult Results(string searchType,string searchTerm)
+        {
+            if (searchType.Equals("all"))
+            {
+                ViewBag.results = JobData.FindByValue(searchTerm);
+            }
+            else
+            {
+                ViewBag.results = JobData.FindByColumnAndValue(searchType, searchTerm);
+                //searchType = column
+                //searchTerm = string / contain
+            }
 
+            return View("Index");
+        }
     }
 }
